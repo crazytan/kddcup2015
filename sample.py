1bd1ff7f4627d822827df9e6e3a43902ef8c70fe
Jia Tan <tjtanjia.tan@gmail.com>
2015-06-19 20:18:21 +0800

    add sample.py; untrack IO.ini

--- sample.py
+++ sample.py
@@ -1,13 +1,40 @@
-import random
-fin = open("fea/train",'r')
-fo1 = open("fea/train_1",'w')
-fo2 = open("fea/train_2",'w')
+#!/usr/bin/env python
 
-for line in fin:
-	if random.uniform(0,1)<0.75:
-		fo1.write(line)
-	else:
-		fo2.write(line)
-fin.close()
-fo1.close()
-fo2.close()
+import random
+
+e_list = []
+with open('raw/enrollment_train.csv', 'r') as fin:
+    f1 = open('raw/enrollment_sample_1.csv', 'w')
+    f2 = open('raw/enrollment_sample_2.csv', 'w')
+
+    title = fin.readline()
+    f1.write(title)
+    f2.write(title)
+    for line in fin:
+        if random.uniform(0, 1) < 0.75:
+            f1.write(line)
+        else:
+            f2.write(line)
+            eid = int(line.strip().split(',')[0])
+            e_list.append(eid)
+
+    f1.close()
+    f2.close()
+
+eset = set(e_list)
+with open('raw/log_train.csv', 'r') as fin:
+    f1 = open('raw/log_sample_1.csv', 'w')
+    f2 = open('raw/log_sample_2.csv', 'w')
+
+    title = fin.readline()
+    f1.write(title)
+    f2.write(title)
+    for line in fin:
+        eid = int(line.strip().split(',', 1)[0])
+        if eid in eset:
+            f2.write(line)
+        else:
+            f1.write(line)
+
+    f1.close()
+    f2.close()
